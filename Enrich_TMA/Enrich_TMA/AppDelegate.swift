44ff6a4930f8b283c09a9424442ed3f9af77fbc8
fkhan <fkhan@workllama.com>
2021-12-02 20:01:50 +0530

    Revert "Test change"
    This reverts commit 09d9350f9d495facd1dcee7b2534743b00741ff7.

--- Enrich_TMA/Enrich_TMA/AppDelegate.swift
+++ Enrich_TMA/Enrich_TMA/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var orientationLock = UIInterfaceOrientationMask.all
     
-    
     var window: UIWindow?
     let customTabbarController = CustomTabbarController.instantiate(fromAppStoryboard: .HomeLanding)
 
